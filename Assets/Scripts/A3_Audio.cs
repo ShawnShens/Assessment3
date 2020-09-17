5ecbcd7298c05a2f632c1fe8fa228f95c8acb446
Shahao-13066206 <Shahao.Shen@student.uts.edu.au>
2020-09-17 22:46:57 +1000

    Feature-Visual Animation and AudioSource Added

---
+++ Assets/Scripts/A3_Audio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A3_Audio : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] audios;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<AudioSource>().clip = audios[0];
+        this.GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!this.GetComponent<AudioSource>().isPlaying)
+        {
+            this.GetComponent<AudioSource>().clip = audios[1];
+            this.GetComponent<AudioSource>().Play();
+        }
+    }
+}
